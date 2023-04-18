ee734a326492f39107f66df256c6460fbe2f4053
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-18 16:09:18 -0700

    moved connect function from play.js to client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setEncoding("utf-8");
+  conn.on("connect", () => {
+    console.log("Connection successful");
+  })
+
+  conn.on("data", () => {
+    console.log("You ded cuz you idled")
+  });
+  return conn;
+}
+
+console.log("Connecting ...");
+
+module.exports = connect;
