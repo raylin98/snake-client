b1f3a117bcab4f12614c53eba479a2e3a9db4c60
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-18 21:59:49 -0700

    added input function

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 const net = require("net");
 
-const connect = require('./client');
+const { connect } = require('./client');
+
+/*const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  stdin.on("data", (handleUserInput) => {
+    if(handleUserInput === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+console.log("Connecting ...");*/
+
+
 
 connect();
