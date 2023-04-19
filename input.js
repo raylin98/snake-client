ada0320fdff1a0182f9633213aee8ebdb0386f9c
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-19 15:27:38 -0700

    added player msgs that are said upon player key input

--- input.js
+++ input.js
@@ -1,5 +1,10 @@
 let connection;
 
+const playerMsg = "Say: ";
+const msg1 = "That apple is mine!";
+const msg2 = "Dibs on the next apple!";
+const msg3 = "I'm winning this";
+
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -14,27 +19,36 @@ const setupInput = function(conn) {
 
 const handleUserInput = function(key) {
   const stdout = process.stdout;
-    if(key === '\u0003') {
-      stdout.write("Exited Game.")
-      process.exit();
-    }
+  if (key === '\u0003') {
+    stdout.write("Exited Game.");
+    process.exit();
+  }
 
-    if(key === "w") {
-      connection.write("Move: up");
-    }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
 
-    if(key === "s") {
-      connection.write("Move: down");
-    }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
 
-    if(key === "a") {
-      connection.write("Move: left");
-    }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
 
-    if(key === "d") {
-      connection.write("Move: right");
-    }
-  };
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "q") {
+    connection.write(playerMsg + msg1);
+  }
+  if (key === "e") {
+    connection.write(playerMsg + msg2);
+  }
+  if (key === "r") {
+    connection.write(playerMsg + msg3);
+  }
+};
 
 
 
